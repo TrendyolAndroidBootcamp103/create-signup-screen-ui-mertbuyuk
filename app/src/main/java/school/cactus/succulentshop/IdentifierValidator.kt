aec6b41dbbd54655846fd0af4ea04337298f2bfc
Mert Büyük <63505261+mertbuyuk@users.noreply.github.com>
2021-05-11 12:26:13 +0300

    Update IdentifierValidator.kt

--- app/src/main/java/school/cactus/succulentshop/IdentifierValidator.kt
+++ app/src/main/java/school/cactus/succulentshop/IdentifierValidator.kt
@@ -13,7 +13,7 @@ class IdentifierValidator : Validator {
     override fun signupValidate(field: String) = when {
         field.isEmpty() -> R.string.email_required
         field.length < 5 || field.length > 50 -> R.string.invalid_email
-        (!field.contains("@") && !field.contains(".")) ->R.string.invalid_email
+        (!field.contains("@") || !field.contains(".")) ->R.string.invalid_email
         else -> null }
 
 
